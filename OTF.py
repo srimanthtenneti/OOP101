79bc8926370fc4261aadabb43f86f21d65d2bd94
Srimanth Tenneti <40752683+srimanthtenneti@users.noreply.github.com>
2020-06-19 16:03:52 +0530

    Update OTF.py

--- OTF.py
+++ OTF.py
@@ -5,5 +5,22 @@ c = calculator() # Class instance
 print("Sum of 1 , 2 is {}".format((c.add(2 , 1))))
 
 """
-Now this describes how we use a class defines in one program call it is another program 
+Now this describes how we use a class defines in one program call it is another program .
+
+Before running the code in this file make sure you delete 
+
+calc = calculator() # Class instance 
+
+a = float(input("Enter a number : "))
+b = float(input("Enter a number :"))
+
+print("\n")
+
+print("Addition of {} & {} is {}\n".format(a,b,calc.add(a,b)))
+print("Subtraction of {} & {} is {}\n".format(a,b,calc.subtract(a,b)))
+print("Multiplication of {} & {} is {}\n".format(a,b,calc.multiply(a,b)))
+print("Division of {} & {} is {}\n".format(a,b,calc.divide(a,b)))
+print("Square root of {} is {}\n".format(a,calc.sqrt(a)))
+
+in the previous file. This is part is added just to shoe the functionality.
 """
