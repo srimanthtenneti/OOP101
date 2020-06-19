5200fec2049160f3784b3ff249d68bdee15031e3
Srimanth Tenneti <40752683+srimanthtenneti@users.noreply.github.com>
2020-06-19 16:01:56 +0530

    Create OTF.py

---
+++ OTF.py
@@ -0,0 +1,9 @@
+from OOP101 import calculator # Calling the calculator class from OOP101 file
+
+c = calculator() # Class instance
+
+print("Sum of 1 , 2 is {}".format((c.add(2 , 1))))
+
+"""
+Now this describes how we use a class defines in one program call it is another program 
+"""
